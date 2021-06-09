10af3dfc868f92c10c0c2e4555e500767c348840
Renalton Filho <renalton@alunos.utfpr.edu.br>
2021-06-09 20:54:31 -0300

    desafio concluído!

---
+++ abecedario.js
@@ -0,0 +1,26 @@
+function solucao(letra, palavras) {
+	let cont = 0;
+    for(let i of palavras){
+      if(i[0] !== letra){
+        cont ++;
+         }
+     }
+ console.log(cont);
+    
+}
+
+function processData(input) {
+	const {letra, palavras} = JSON.parse(input)[0];
+    solucao(letra, palavras);
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
